2c0529f8a1e853d5be46013cf4e3433cc8c4154a
aleksandrspiridonov <aleksandr@spiridonov.us>
2018-07-18 13:02:13 -0400

    Added firebase angular

---
+++ src/environments/firebase.secrt.template.ts
@@ -0,0 +1,8 @@
+export const firebaseSecret = {
+    apiKey: ":)",
+    authDomain: "aleks-box-o-air-superstore.firebaseapp.com",
+    databaseURL: "https://aleks-box-o-air-superstore.firebaseio.com",
+    projectId: "aleks-box-o-air-superstore",
+    storageBucket: "aleks-box-o-air-superstore.appspot.com",
+    messagingSenderId: ":)"
+}
