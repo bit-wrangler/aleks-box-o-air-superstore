dc3b37c370a3770ac8ec4d232ce2b6ad77ec9456
aleksandrspiridonov <aleksandr@spiridonov.us>
2018-07-28 12:34:01 -0400

    implemented basic product card component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-
+  @Input('title')
+  title: string;
+  @Input('price')
+  price: number;
+  @Input('img-src')
+  imgSrc: string;
+  
   constructor() { }
 
   ngOnInit() {
